fd83442cef1b8c6eaf604c172a86d9e0b0e1bb18
makmacedo <arcmak@gmail.com>
2018-09-25 12:39:10 +0100

    First Steps (1)

---
+++ application/views/posts/index.php
@@ -0,0 +1,3 @@
+<h2><?= $title ?></h2>
+
+<script>$('#blog').addClass("active");</script>
---
+++ application/views/templates/header.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <title>CI Project</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/slate/bootstrap.min.css">
+        <!-- demo and components : https://bootswatch.com/slate/-->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">CI Project</a>
+            <ul class="navbar-nav">
+            <li class="nav-item" id="home"><a class="nav-link" href="<?php echo base_url();?>">Home <span class="sr-only">(current)</span></a></li>
+            <li class="nav-item" id="about"><a class="nav-link" href="<?php echo base_url();?>about">About</a></li>
+            <li class="nav-item" id="blog"><a class="nav-link" href="<?php echo base_url();?>posts">Blog</a></li>
+            </ul>
+    </nav>
+    <div class="container">
